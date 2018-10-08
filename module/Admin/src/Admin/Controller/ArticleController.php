4d8763124d8f28e3f737233ae430127f2010b8b1
vladimir <vladimir@overnightglasses.com>
2018-10-08 13:20:02 +0300

    add edit article

--- module/Admin/src/Admin/Controller/ArticleController.php
+++ module/Admin/src/Admin/Controller/ArticleController.php
@@ -66,4 +66,57 @@ class ArticleController extends BaseController
 
         return $this->redirect()->toRoute('admin/article');
     }
+
+    public function editAction(){
+
+        $status = $message = '';
+        $em = $this->getEntityManager();
+        $form = new ArticleAddForm($em);
+        $id = (int) $this->params()->fromRoute('id', 0);
+        $article = $em->find('Blog\Entity\Article', $id);
+
+        if(empty($article)){
+            $message = 'Статья не найденна';
+            $status = 'error';
+            $this->flashMessenger()->setNamespace($status)->addMessage($message);
+            return $this->redirect()->toRoute('admin/edit');
+
+        }
+
+        $form->setHydrator(new DoctrineHydrator($em, '\Article'));
+        $form->bind($article);
+
+        $request = $this->getRequest();
+
+        if($request->isPost()){
+
+            $data = $request->getPost();
+            $form->setData($data);
+
+            if($form->isValid()){
+                $em->persist($article);
+                $em->flush();
+
+                $status = 'success';
+                $message = 'Статья измененна';
+            } else {
+                $status = 'error';
+                $message = 'Ошибка параметров';
+                foreach ($form->getInputFilter()->getInvalidInput() as $errors){
+                    foreach ($errors->getMessages() as $error){
+                        $message .= ' ' . $error;
+                    }
+                }
+
+            }
+        } else {
+            return ['form' =>$form, 'id' => $id];
+        }
+
+        if($message){
+            $this->flashMessenger()->setNamespace($status)->addMessage($message);
+        }
+
+        return $this->redirect()->toRoute('admin/article');
+    }
 }
